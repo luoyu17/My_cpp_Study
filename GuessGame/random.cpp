2ec38576e83db09688bdec4ef627099a577f0263
Luo Yu <luoyuyx@gmail.com>
2016-12-26 11:00:58 +0800

    practice for game
    guess game

---
+++ GuessGame/random.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void Randomize(){
+	srand( (int)time(NULL) );
+}
+
+int GenerateRandomNum(int low,int high){
+	double d;
+	if (low > high)
+	{
+		cout<<"Please make sure low <= high.\n";
+		exit(1);
+	}
+	d = (double)rand() / ( (double)RAND_MAX+1.0);
+
+	return (low + (int)(d*(high-low+1)));
+}
+
+double GenerateRandomRea(double low,double high){
+	double d;
+	if (low > high)
+	{
+		cout<<"Please make sure low <= high.\n";
+		exit(2);
+	}
+	
+	d = (double)rand() / (double)RAND_MAX;
+
+	return ( low + d*(high-low) );
+}
